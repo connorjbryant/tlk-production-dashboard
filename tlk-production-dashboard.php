8f03606e8af010ce47e4ed26b03ddafe53b75e9e
Connor Bryant <cbryant@TLK.Local>
2026-09-16 14:19:32 -0400

    Version number update

--- tlk-production-dashboard.php
+++ tlk-production-dashboard.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: TLK Production Dashboard
  * Description: Production dashboard for TLK Precision
- * Version: 1.3.6
+ * Version: 1.3.7
  * Author: Connor Bryant
  * License: GPL-2.0+
  */
@@ -18,7 +18,7 @@ function tlk_dash_enqueue_assets(){
         'tlk_dash_styles',
         plugins_url('css/tlk-dash.css', __FILE__),
         array(),
-        '1.3.6',
+        '1.3.7',
         'all'
     );
 
@@ -27,7 +27,7 @@ function tlk_dash_enqueue_assets(){
         'tlk_dash_script',
         plugins_url('js/tlk-dash.js', __FILE__),
         array('jquery'),
-        '1.3.6',
+        '1.3.7',
         true
     );
 }
