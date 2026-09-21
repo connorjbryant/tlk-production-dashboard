3faefe7f019aed389a4a2bded558d4c05d3ef77a
Connor Bryant <cbryant@TLK.Local>
2026-09-21 09:54:10 -0400

    TV style update

--- tlk-production-dashboard.php
+++ tlk-production-dashboard.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: TLK Production Dashboard
  * Description: Production dashboard for TLK Precision
- * Version: 1.8.4
+ * Version: 1.8.5
  * Author: Connor Bryant
  * License: GPL-2.0+
  */
@@ -24,7 +24,7 @@ function tlk_dash_enqueue_assets(){
         return;
     }
 
-    $version = '1.8.4';
+    $version = '1.8.5';
 
     wp_enqueue_style(
         'tlk_dash_styles',
